d516c490bc730d04df82663491629621ad43ede1
Lê Văn Lưu <87215931+luuDevBiker@users.noreply.github.com>
2022-05-12 14:50:04 +0700

    create file git

---
+++ lession1.py
@@ -0,0 +1,13 @@
+'''
+                LESSION 1 :
+    introduce about print , input method and declare variable in python
+( giới thiệu về hàm nhập xuất và khai báo biến trong python )
+
+'''
+
+''' 
+1 : Khai báo biến 
+trong python 
+'''
+
+
